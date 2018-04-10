40eaaf0e7a09bffe807de0c56632c4a40b75e952
Stefanos Mitropoulos <stefmitropoulos@gmail.com>
2018-04-10 10:55:12 +0300

    better visualization

--- Machine Learning A-Z/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
@@ -22,26 +22,21 @@ X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
-# Fitting Simple Linear Regression to the Training set
+#Fitting simple linear regression to the training set.
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
-regressor.fit(X_train, y_train)
+regressor.fit(X_train,y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
 
-# Visualising the Training set results
-plt.scatter(X_train, y_train, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Training set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
-plt.show()
 
-# Visualising the Test set results
-plt.scatter(X_test, y_test, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Test set)')
+# Visualising the set results
+plt.scatter(X_train, y_train, color = 'blue') #Plot the train data set points
+plt.scatter(X_test, y_test, color = 'red')  #Plot the test data set points
+plt.scatter(X_test, y_pred, color = 'green') #Plot the predicted data set points
+plt.plot(X_train, regressor.predict(X_train), color = 'yellow')
+plt.title('Salary vs Experience')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.show()

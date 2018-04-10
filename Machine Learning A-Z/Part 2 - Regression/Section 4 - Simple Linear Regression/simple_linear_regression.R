535bfc51184ebeb7fdd119ebc2a3374e12462276
Stefanos Mitropoulos <stefmitropoulos@gmail.com>
2018-04-10 15:52:03 +0300

    better visuals

--- Machine Learning A-Z/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.R
+++ Machine Learning A-Z/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.R
@@ -4,8 +4,6 @@
 dataset = read.csv('Salary_Data.csv')
 
 # Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
-library(caTools)
 set.seed(123)
 split = sample.split(dataset$Salary, SplitRatio = 2/3)
 training_set = subset(dataset, split == TRUE)
@@ -22,24 +20,18 @@ regressor = lm(formula = Salary ~ YearsExperience,
 # Predicting the Test set results
 y_pred = predict(regressor, newdata = test_set)
 
-# Visualising the Training set results
-library(ggplot2)
-ggplot() +
-  geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary),
-             colour = 'red') +
-  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
-            colour = 'blue') +
-  ggtitle('Salary vs Experience (Training set)') +
-  xlab('Years of experience') +
-  ylab('Salary')
 
 # Visualising the Test set results
 library(ggplot2)
 ggplot() +
+  geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary),
+             colour = 'green') +  #training set points in green
   geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary),
-             colour = 'red') +
+             colour = 'red') +    #testing set points in green
+  geom_point(aes(x = test_set$YearsExperience, y = y_pred),
+             colour = 'orange') + #regression points in orange
   geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
-            colour = 'blue') +
-  ggtitle('Salary vs Experience (Test set)') +
+            colour = 'blue') +  #regression line in blue
+  ggtitle('Salary vs Experience\n') +
   xlab('Years of experience') +
   ylab('Salary')

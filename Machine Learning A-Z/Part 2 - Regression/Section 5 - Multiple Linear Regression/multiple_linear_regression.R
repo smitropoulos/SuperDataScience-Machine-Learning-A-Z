9196dbe85dee5f1ec83dcd42fc299328672ce7bb
Stefanos Mitropoulos <stefmitropoulos@gmail.com>
2018-04-11 14:40:22 +0300

    Final commenting and backward elimination implementation (plus auto)

--- Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.R
+++ Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.R
@@ -42,3 +42,45 @@ y_pred = predict(regressor, newdata = test_set)
 
 # Pretty good results, but still the only thing we did, was eliminate the variables not statistically relevant to
 # the result and end up with a linear regressor of the form ax+b.
+
+
+# Fitting Multiple Linear Regression to the Training set Using Backward Compatibility.
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State,
+               data = dataset)  #Notice we use the whole dataset instead of the training_set now. Not necessary really.
+
+# Fitting Multiple Linear Regression to the Training set Using Backward Compatibility.
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend,
+               data = dataset)
+summary(regressor)
+
+# Fitting Multiple Linear Regression to the Training set Using Backward Compatibility.
+regressor = lm(formula = Profit ~ R.D.Spend + Marketing.Spend,
+               data = dataset)
+summary(regressor)
+
+# Fitting Multiple Linear Regression to the Training set Using Backward Compatibility.
+regressor = lm(formula = Profit ~ R.D.Spend,
+               data = dataset)
+summary(regressor)
+
+
+##AUTOMATIC BACKWARD ELIMINATION IMPLEMENTATION.
+
+# backwardElimination <- function(x, sl) {
+#   numVars = length(x)
+#   for (i in c(1:numVars)){
+#     regressor = lm(formula = Profit ~ ., data = x)
+#     maxVar = max(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"])
+#     if (maxVar > sl){
+#       j = which(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"] == maxVar)
+#       x = x[, -j]
+#     }
+#     numVars = numVars - 1
+#   }
+#   return(summary(regressor))
+# }
+# 
+# SL = 0.05
+# dataset = dataset[, c(1,2,3,4,5)]
+# backwardElimination(training_set, SL)
+

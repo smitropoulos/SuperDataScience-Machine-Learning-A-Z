7c8f1bbd9bc7b216baa4e72ccb9372eddd154fcd
Stefanos Mitropoulos <stefmitropoulos@gmail.com>
2018-04-11 14:17:06 +0300

    Commenting on R and polishing.

--- Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.R
+++ Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.R
@@ -9,20 +9,36 @@ dataset$State = factor(dataset$State,
                        labels = c(1, 2, 3))
 
 # Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
 library(caTools)
 set.seed(123)
 split = sample.split(dataset$Profit, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
-
 # Fitting Multiple Linear Regression to the Training set
-regressor = lm(formula = Profit ~ .,
+regressor = lm(formula = Profit ~.,
                data = training_set)
+
+#***Important Notice****
+#R implemented Dummy variables by itself AND avoided the dummy variable trap. (Only included State2 and State3 in the fit.)
+# Residuals:
+#   Min     1Q Median     3Q    Max 
+# -33128  -4865      5   6098  18065 
+# 
+# Coefficients:
+#   Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)      4.965e+04  7.637e+03   6.501 1.94e-07 ***
+#   R.D.Spend        7.986e-01  5.604e-02  14.251 6.70e-16 ***
+#   Administration  -2.942e-02  5.828e-02  -0.505    0.617    
+# Marketing.Spend  3.268e-02  2.127e-02   1.537    0.134    
+# State2           1.213e+02  3.751e+03   0.032    0.974    
+# State3           2.376e+02  4.127e+03   0.058    0.954    
+# ---
+#   Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
+ 
 
 # Predicting the Test set results
 y_pred = predict(regressor, newdata = test_set)
+
+# Pretty good results, but still the only thing we did, was eliminate the variables not statistically relevant to
+# the result and end up with a linear regressor of the form ax+b.
--- Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -42,9 +42,87 @@ Although some variables may not be statistically relevant. This is where the bac
 
 """
 
-#Backward Compatibility start
+#Backward Compatibility start. STEP1: We declare an SL of 5%
 import statsmodels.formula.api as sm
+#Appending to an array of ones the array X in order to have a column of 1s in the front.
+X = np.append(arr = np.ones((50,1)).astype(int),values = X, axis=1)
+
+X_opt = X [:, [0,1,2,3,4,5]]
+regressor_OLS = sm.OLS(y,X_opt).fit()   #STEP2: done
+regressor_OLS.summary() #Useful to introduce information on the regressor
+
+#x2 has a 99% P-value, hence we need to remove it and refit the model. STEP3: done
+X_opt = X [:, [0,1,3,4,5]] #STEP4: done
+regressor_OLS = sm.OLS(y,X_opt).fit() #STEP5: done
+regressor_OLS.summary()
+
+X_opt = X [:, [0,3,4,5]] #STEP4: done
+regressor_OLS = sm.OLS(y,X_opt).fit() #STEP5: done
+regressor_OLS.summary()
+
+X_opt = X [:, [0,3,5]] #STEP4: done
+regressor_OLS = sm.OLS(y,X_opt).fit() #STEP5: done
+regressor_OLS.summary()
+X_opt = X [:, [0,3]] #STEP4: done
+regressor_OLS = sm.OLS(y,X_opt).fit() #STEP5: done
+regressor_OLS.summary()
+#Here we exit the loop of backward elimination since we now have all of our variables with P<SL.
+
+"""
+
+#This is an automatic implementation of the backward elimination algorithm. with p-values only:
 
 
 
+import statsmodels.formula.api as sm
+def backwardElimination(x, sl):
+    numVars = len(x[0])
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        if maxVar > sl:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    x = np.delete(x, j, 1)
+    regressor_OLS.summary()
+    return x
 
+SL = 0.05
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+X_Modeled = backwardElimination(X_opt, SL)
+"""
+
+
+"""
+#Backward Elimination with p-values and Adjusted R Squared:
+
+import statsmodels.formula.api as sm
+def backwardElimination(x, SL):
+    numVars = len(x[0])
+    temp = np.zeros((50,6)).astype(int)
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        adjR_before = regressor_OLS.rsquared_adj.astype(float)
+        if maxVar > SL:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    temp[:,j] = x[:, j]
+                    x = np.delete(x, j, 1)
+                    tmp_regressor = sm.OLS(y, x).fit()
+                    adjR_after = tmp_regressor.rsquared_adj.astype(float)
+                    if (adjR_before >= adjR_after):
+                        x_rollback = np.hstack((x, temp[:,[0,j]]))
+                        x_rollback = np.delete(x_rollback, j, 1)
+                        print (regressor_OLS.summary())
+                        return x_rollback
+                    else:
+                        continue
+    regressor_OLS.summary()
+    return x
+
+SL = 0.05
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+X_Modeled = backwardElimination(X_opt, SL)
+
+"""
